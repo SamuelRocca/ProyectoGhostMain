0bad725e17b54faea5556caa71a955f0f90d9483
Samuel Rocha <Samuelandres_rocha@unitec.edu>
2020-03-12 14:16:51 -0600

    Commit

--- Proyecto_Ghost/src/proyecto_ghost/Fantasmas.java
+++ Proyecto_Ghost/src/proyecto_ghost/Fantasmas.java
@@ -2,16 +2,21 @@
 package proyecto_ghost;
 
 public class Fantasmas {
-    
-        String tipo;
         
+        
+        Player jugador;
+        String fantasmaJugador;
+        String tipo;
+        int cantidad = 8;
     
-        public Fantasmas(String tipo)
+        public Fantasmas (Player jugador, String numJugador, String tipo)
         {
             this.tipo = tipo;
+            this.jugador = jugador;
+            fantasmaJugador = "F" + numJugador;
         }
         
-        public void moverse ()
+        public void moverse (int x, int y)
         {
             
         }
